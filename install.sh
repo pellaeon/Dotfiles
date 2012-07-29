53f75e99170da04fcc3795c8a9d83c4ba45dc637
Pellaeon Lin <pellaeon@hs.ntnu.edu.tw>
2012-07-29 15:23:57 +0800

    cshrc

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ ln -s ~/Dotfiles/bashrc ~/.bashrc
 ln -s ~/Dotfiles/screenrc ~/.screenrc
 ln -s ~/Dotfiles/inputrc ~/.inputrc
 ln -s ~/Dotfiles/gitconfig ~/.gitconfig
+ln -s ~/Dotfiles/cshrc ~/.cshrc
+ln -s ~/Dotfiles/complete ~/.complete
